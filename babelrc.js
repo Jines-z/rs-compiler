e2ea1607325cab5e191d282b563ac94b2b7ae378
郑傲今 <zhengaj@shinemo.com>
2019-10-11 10:32:34 +0800

    fix: Compilation failed

--- babelrc.js
+++ babelrc.js
@@ -13,7 +13,6 @@ module.exports = {
     ],
     plugins: [
         ['@babel/plugin-transform-runtime'],
-        ['@babel/plugin-transform-typescript', { allowNamespaces: true }],
         ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-proposal-class-properties', { loose: true }],
         ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
